ba18bdb110cc8417d97b99daa77958f205b2a4e3
Hudi304 <hudisteanum@yahoo.com>
2023-02-25 20:27:20 +0200

    basic ota poc

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <WiFiUdp.h>
 
 // #define WIFI_SSID "TechQuarter"
 // #define WIFI_PASSWORD "!techP455"
@@ -31,28 +32,99 @@ DallasTemperature sensors(&oneWire);
 WiFiServer server(80);
 Application app;
 
+// void setup()
+// {
+//   pinMode(LED_BUILTIN, OUTPUT);
+//   pinMode(RELAY_PIN, OUTPUT);
+
+//   digitalWrite(LED_BUILTIN, 0);
+
+//   Serial.begin(115200);
+//   connect_to_wifi(WIFI_SSID, WIFI_PASSWORD);
+//   configure_app(app);
+//   server.begin();
+//   sensors.begin();
+
+//   // if (!MDNS.begin("thermostat"))
+//   // {
+//   //   Serial.println("Error starting mDNS");
+//   //   return;
+//   // }
+
+//   MDNS.begin("esp32");
+//   MDNS.addService("_http", "_tcp", 80);
+//   MDNS.addServiceTxt("_http", "_tcp", "board", "ESP32");
+
+//   // Port defaults to 3232
+//   // ArduinoOTA.setPort(3232);
+
+//   // Hostname defaults to esp3232-[MAC]
+//   // ArduinoOTA.setHostname("myesp32");
+
+//   // No authentication by default
+//   // ArduinoOTA.setPassword("admin");
+
+//   // Password can be set with it's md5 value as well
+//   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+//   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+//   ArduinoOTA.setHostname(server_url);
+
+//   ArduinoOTA
+//       .onStart([]()
+//                {
+//       String type;
+//       if (ArduinoOTA.getCommand() == U_FLASH)
+//         type = "sketch";
+//       else // U_SPIFFS
+//         type = "filesystem";
+
+//       // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+//       Serial.println("Start updating " + type); })
+//       .onEnd([]()
+//              { Serial.println("\nEnd"); })
+//       .onProgress([](unsigned int progress, unsigned int total)
+//                   { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+//       .onError([](ota_error_t error)
+//                {
+//       Serial.printf("Error[%u]: ", error);
+//       if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+//       else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+//       else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+//       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+//       else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
+
+//   ArduinoOTA.begin();
+// }
+// void loop()
+// {
+//   //! Do not delete
+//   millis_sec();
+//   process_web_client(server, app);
+//   check_temp(ambient_t_C, sensors);
+//   ArduinoOTA.handle();
+// }
+
+// variabls for blinking an LED with Millis
+const int led = 2;                // ESP32 Pin to which onboard LED is connected
+unsigned long previousMillis = 0; // will store last time LED was updated
+const long interval = 1000;       // interval at which to blink (milliseconds)
+int ledState = LOW;               // ledState used to set the LED
+
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(RELAY_PIN, OUTPUT);
 
-  digitalWrite(LED_BUILTIN, 0);
-
+  pinMode(led, OUTPUT);
   Serial.begin(115200);
-  connect_to_wifi(WIFI_SSID, WIFI_PASSWORD);
-  configure_app(app);
-  server.begin();
-  sensors.begin();
-
-  // if (!MDNS.begin("thermostat"))
-  // {
-  //   Serial.println("Error starting mDNS");
-  //   return;
-  // }
-
-  MDNS.begin("esp32");
-  MDNS.addService("_http", "_tcp", 80);
-  MDNS.addServiceTxt("_http", "_tcp", "board", "ESP32");
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
 
   // Port defaults to 3232
   // ArduinoOTA.setPort(3232);
@@ -66,8 +138,6 @@ void setup()
   // Password can be set with it's md5 value as well
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA.setHostname(server_url);
 
   ArduinoOTA
       .onStart([]()
@@ -94,12 +164,24 @@ void setup()
       else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
 
   ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
+
 void loop()
 {
-  //! Do not delete
-  millis_sec();
-  process_web_client(server, app);
-  check_temp(ambient_t_C, sensors);
+  // loop to blink without delay
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    // if the LED is off turn it on and vice-versa:
+    ledState = not(ledState);
+    // set the LED with the ledState of the variable:
+    digitalWrite(led, ledState);
+  }
   ArduinoOTA.handle();
 }
