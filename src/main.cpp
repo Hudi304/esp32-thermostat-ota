d8dd682fc9b83ba98060f5351275e5165a135eb7
Hudi304 <hudisteanum@yahoo.com>
2023-03-05 22:46:16 +0200

    Add web Sockets to the old app [Working, NEEDS REFACTOR]

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@
 #include "socket_controller.h"
 
 // Import required libraries
-#include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include "credentials.h"
@@ -26,77 +25,81 @@
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 // // does this do anything?
-WiFiServer server(80);
+WiFiServer server(81);
 Application app;
 
 bool ledState = 0;
 const int ledPin = 2;
 
-// Create AsyncWebServer object on port 80
-// AsyncWebServer async_web_server(81);
-// AsyncWebSocket ws("/ws");
+// Create AsyncWebServer object on port 81
+AsyncWebServer async_web_server(80);
+AsyncWebSocket ws("/ws");
 
-// void notifyClients()
-// {
-//   ws.textAll(String(ledState));
-// }
 
-// void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
-// {
-//   AwsFrameInfo *info = (AwsFrameInfo *)arg;
-//   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
-//   {
-//     data[len] = 0;
-//     if (strcmp((char *)data, "toggle") == 0)
-//     {
-//       ledState = !ledState;
-//       notifyClients();
-//     }
-//   }
-// }
+void notifyClients()
+{
+  ws.textAll(String(ledState));
+}
 
-// void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-//              void *arg, uint8_t *data, size_t len)
-// {
-//   switch (type)
-//   {
-//   case WS_EVT_CONNECT:
-//     Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-//     break;
-//   case WS_EVT_DISCONNECT:
-//     Serial.printf("WebSocket client #%u disconnected\n", client->id());
-//     break;
-//   case WS_EVT_DATA:
-//     handleWebSocketMessage(arg, data, len);
-//     break;
-//   case WS_EVT_PONG:
-//   case WS_EVT_ERROR:
-//     break;
-//   }
-// }
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+  {
+    data[len] = 0;
+    if (strcmp((char *)data, "toggle") == 0)
+    {
+      ledState = !ledState;
+      notifyClients();
+    }
+  }
+}
 
-// void initWebSocket()
-// {
-//   ws.onEvent(onEvent);
-//   async_web_server.addHandler(&ws);
-// }
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len)
+{
+  switch (type)
+  {
+  case WS_EVT_CONNECT:
+    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    break;
+  case WS_EVT_DISCONNECT:
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    break;
+  case WS_EVT_DATA:
+    handleWebSocketMessage(arg, data, len);
+    break; // Route for root / web page
+    // async_web_server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+    // { request->send_P(200, "text/html", index_html, processor); });
 
-// String processor(const String &var)
-// {
-//   Serial.println(var);
-//   if (var == "STATE")
-//   {
-//     if (ledState)
-//     {
-//       return "ON";
-//     }
-//     else
-//     {
-//       return "OFF";
-//     }
-//   }
-//   return String();
-// }
+  case WS_EVT_PONG:
+  case WS_EVT_ERROR:
+    break;
+  }
+}
+
+void initWebSocket()
+{
+  ws.onEvent(onEvent);
+  async_web_server.addHandler(&ws);
+}
+
+String processor(const String &var)
+{
+  Serial.println(var);
+  if (var == "STATE")
+  {
+    if (ledState)
+    {
+      return "ON";
+    }
+    else
+    {
+      return "OFF";
+    }
+  }
+  return String();
+}
 
 void setup()
 {
@@ -105,7 +108,7 @@ void setup()
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, 0);
+  digitalWrite(LED_BUILTIN, 1);
   Serial.begin(115200);
   // wifi connection
   connect_to_wifi(WIFI_SSID, WIFI_PASSWORD);
@@ -116,19 +119,24 @@ void setup()
   sensors.begin();
   server.begin();
 
-  // initWebSocket();
-  init_state_machine(sensors);
+  delay(100);
 
+  init_state_machine(sensors);
+  initWebSocket();
   // Route for root / web page
-  // async_web_server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-  // { request->send_P(200, "text/html", index_html, processor); });
+  async_web_server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+                      { request->send_P(200, "text/html", index_html, processor); });
 
   // Start server
-  // async_web_server.begin();
+  async_web_server.begin();
 }
 
 void loop()
 {
-  // ws.cleanupClients();
+  ws.cleanupClients();
+  compute_seconds_since_on();
+  process_web_client(server, app);
+  check_temp(ambient_t_C, sensors);
+
   digitalWrite(ledPin, ledState);
 }
--- src/old_main.h
+++ src/old_main.h
@@ -1,3 +1,36 @@
+#include <Arduino.h>
+#include <aWOT.h>
+#include <OneWire.h>
+#include <WiFi.h>
+#include <DallasTemperature.h>
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <stdio.h>
+#include <WiFiUdp.h>
+
+#include "common.h"
+#include "variables.h"
+#include "temp.h"
+#include "web_controller.h"
+#include "mDNS.h"
+#include "ota.h"
+#include "socket_controller.h"
+
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#include "credentials.h"
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+// does this do anything?
+WiFiServer server(80);
+Application app;
+
+// we might not need the other application at all
+AsyncWebServer async_server(81);
+AsyncWebSocket ws("/ws");
+
 void setup()
 {
   // Pins
@@ -14,20 +47,21 @@ void setup()
   sensors.begin();
 
   // you can find the app at http://thermostat.local
-  // set_up_mDNS("thermostat_dev");
+  set_up_mDNS("thermostat");
   // sets up a simple over the air (OTA) config
-  // set_up_OTA("thermostat_dev");
+  set_up_OTA("thermostat");
 
   delay(100);
 
-  // initWebSocket(&ws, &async_server);
+  init_state_machine(sensors);
+
+  initWebSocket(&ws, &async_server);
 }
 
 void loop()
 {
   compute_seconds_since_on();
-  // process_web_client(server, app);
-  // check_temp(ambient_t_C, sensors);
-  // ArduinoOTA.handle();
-  ws.cleanupClients();
+  process_web_client(server, app);
+  check_temp(ambient_t_C, sensors);
+  ArduinoOTA.handle();
 }
